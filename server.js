b9db716f760efd8559a0600856285aa9d36bcb7d
My Phung Tieu <tmp03099@gmail.com>
2023-04-06 16:00:05 -0400

    Tip Calculator express application created.

--- server.js
+++ server.js
@@ -15,6 +15,12 @@ app.get("/greeting/:name", (req, res) => {
   res.send(`Wow! What's up ${req.params.name}`);
 });
 
+app.get("/tip/:total/:tipPercentage", (req, res) => {
+  console.log(req.params);
+  const { total, tipPercentage } = req.params;
+  res.send(`How much your tip will be ${tipPercentage}`);
+});
+
 app.listen(3000, function () {
   console.log("Listening on port 3000");
 });
